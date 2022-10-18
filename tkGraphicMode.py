9276f6a79ed39d99558989f003cf152bcccd2bac
Enzo Gabard <enzo.gabard@etu.u-bordeaux.fr>
2022-10-18 13:28:44 +0200

    avancée de l'interface graphique

--- tkGraphicMode.py
+++ tkGraphicMode.py
@@ -1,18 +1,30 @@
 import tkinter as tk
 from GameState import *
 
-class GameWindow(tk.Tk):
-    def __init__ (self,width = 1080, height = 720, title = "Jeu des carrés"):
-        tk.Tk().__init__(self)
+class GameWindow():
+    def __init__ (self,width = 1080, height = 720, title = "Jeu des carrés",root = tk.Tk()):
+        self.root = root
         self.width = width
         self.height = height
         self.title = title
         
-        self.init()
-    
-    def init(self):
-        self.title(self.title)
-        self.dimension(f"{self.width}x{self.height}")
-        self.flip()
+        self.init_window()
+        self.select_dimensions()
         
+    def init_window(self):
+        self.root.title(self.title)
+        self.root.geometry(f"{self.width}x{self.height}")
+        self.root.resizable(False,False)
+        
+    def select_dimensions(self):
+        width_text = tk.Label("Test")
+        width_text.place(relwidth = 0.05,relheight = 0.05,relx = 1-0.25-0.05,rely = 0.425)
+        width = tk.Entry()
+        height = tk.Entry()
+        height.place(relwidth = 0.05,relheight = 0.05,relx = 1-0.25-0.05,rely = 0.425)
+        width.place(relwidth = 0.05,relheight = 0.05,relx = 0.25,rely = 0.425)
+        self.root.update()
+        
+    def init_board(self):
+        pass
 GW = GameWindow()
