4f48c9198ec20ddb96f2dbc89d4c867963757056
Enzo Gabard <enzo.gabard@etu.u-bordeaux.fr>
2022-10-25 12:19:38 +0200

    avancée sur le graphique

--- tkGraphicMode.py
+++ tkGraphicMode.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from GameState import *
+import time
 
 class GameWindow():
     def __init__ (self,width = 1080, height = 720, title = "Jeu des carrés",root = tk.Tk()):
@@ -7,7 +8,10 @@ class GameWindow():
         self.width = width
         self.height = height
         self.title = title
-
+        
+        self.point_positions_list = []
+        self.line_list = []
+        
         self.init_window()
         self.start()
         
@@ -56,26 +60,53 @@ class GameWindow():
     def start_game(self):
         self.destroy_after_dimensions()
         
-        self.draw_grid()
-        
-    def draw_grid(self):
+        self.init_board()
+    
+    def draw_grid_lines(self):
+        for y in range(self.grid_height+1):
+            for x in range(self.grid_width+1):
+                point_position = self.find_point_position(x,y)
+                if point_position != False:
+                    if point_position[0] + self.square_border_length - 2 <= self.x_decalage + self.square_border_length * self.grid_width:
+                        self.line_list.append(self.board.create_line(point_position[0]+6,point_position[1]+2,point_position[0]+self.square_border_length-2,point_position[1]+2,width = 5))
+                    if point_position[3] + self.square_border_length - 2 <= self.y_decalage + self.square_border_length * self.grid_height:
+                        self.line_list.append(self.board.create_line(point_position[0]+2,point_position[1]+6,point_position[0]+2,point_position[1]+self.square_border_length - 2,width = 5))
+    def find_point_position(self,x,y):
+        for point in self.point_positions_list:
+            if (x,y)==point[0]:
+                return point[1]
+        return False
+    
+    def draw_grid_points(self):
          
         
-        canvas_height = int(0.8 * self.height)
-        canvas_width = int(0.8 * self.height)
+        self.canvas_height = int(0.8 * self.height)
+        self.canvas_width = int(0.8 * self.height)
         
-        square_height = canvas_height // self.grid_height
-        square_width = canvas_width // self.grid_width
+        square_height = self.canvas_height // self.grid_height
+        square_width = self.canvas_width // self.grid_width
         
-        self.board = tk.Canvas(self.root, width = canvas_width, height = canvas_height, bg = 'black')
+        self.board = tk.Canvas(self.root, width = self.canvas_width, height = self.canvas_height, bg = 'white')
         self.board.place(relx= 0.25, rely = 0.0)
         
-        square_border_length = min(square_height,square_width)
+        self.square_border_length = min(square_height,square_width)
+        self.x_decalage = (self.canvas_width-self.grid_width*self.square_border_length)/2
+        self.y_decalage = (self.canvas_height-self.grid_height*self.square_border_length)/2
         
         for x in range(0, self.grid_width+1):
             for y in range (0, self.grid_height+1):
-                self.board.create_oval(x*square_border_length-5,y*square_border_length-5,x*square_border_length+5,y*square_border_length+5,fill = 'white')
-        
+                x0 = self.x_decalage+(x*self.square_border_length)-2
+                x1 = self.x_decalage+(x*self.square_border_length)+2
+                y0 = self.y_decalage+(y*self.square_border_length)-2
+                y1 = self.y_decalage+(y*self.square_border_length)+2
+                
+                self.board.create_oval(x0,y0,x1,y1,fill = 'green')
+                
+                point = (x,y)
+                point_position = (x0,y0,x1,y1)
+                self.point_positions_list.append((point,point_position))
+                self.board.update()
+
     def destroy_after_dimensions(self):
         self.width_text.destroy()
         self.height_text.destroy()
@@ -86,7 +117,10 @@ class GameWindow():
         self.selection_sentence.destroy()
         
     def init_board(self):
-        pass
+        self.draw_grid_points()
+        self.draw_grid_lines()
     
+    
+
 if __name__ == '__main__':
     GW = GameWindow()
